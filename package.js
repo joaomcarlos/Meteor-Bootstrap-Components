f34d38e4a5bc80408080c2deb44ae2383a668b67
Joao Carlos <thesilentwarrior@gmail.com>
2013-12-14 18:35:52 +0000

    fixed breadcrumbs

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.on_use(function (api) {
 
   var path = Npm.require('path');
   var component_path = path.join('components');
-  var components = ["panel", "alert", "label", "glyphicon"];
+  var components = ["panel", "alert", "label", "glyphicon","breadcrumb"];
 
   for(var i=0; i < components.length; i++){
     api.add_files(path.join(component_path, components[i], 'interface.html'), 'client');
