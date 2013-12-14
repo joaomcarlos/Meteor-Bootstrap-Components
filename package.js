edb3fa867a8efed23352ec504f408d486e98e284
Jake Gaylor <jhgaylor@gmail.com>
2013-12-14 06:54:18 -0600

    oops

--- package.js
+++ package.js
@@ -10,9 +10,9 @@ Package.on_use(function (api) {
   var components = ["panel"];
 
   for(var i=0; i < components.length; i++){
-    api.add_files(path.join(asset_path, components[i], 'logic.coffee'), 'client');
     api.add_files(path.join(asset_path, components[i], 'interface.html'), 'client');
     api.add_files(path.join(asset_path, components[i], 'example.html'), 'client');
+    api.add_files(path.join(asset_path, components[i], 'logic.coffee'), 'client');
   }
 
   api.add_files('demo.html', 'client');
