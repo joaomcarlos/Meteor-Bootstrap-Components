6f71ab0c5543ed2104ac1d7c85b194901aa1056b
Joao Carlos <thesilentwarrior@gmail.com>
2013-12-14 15:33:40 +0000

    added testing for panel and coffeescript to package.js

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['jquery', 'bootstrap-3', 'templating'], 'client');
+  api.use(['jquery', 'bootstrap-3', 'templating', 'coffeescript'], 'client');
 
   var path = Npm.require('path');
   var component_path = path.join('components');
